c14b3b18b850d6d941f44f74584eec78f52bcd95
MaximeArenas <81745672+MaximeArenas@users.noreply.github.com>
2021-05-23 14:02:03 +0200

    Update Point.java

--- src/main/java/fr/insa/bathily/projettreillis2/Point.java
+++ src/main/java/fr/insa/bathily/projettreillis2/Point.java
@@ -3,56 +3,78 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+package fr.insa.bathily.projettreillis2;
 
-/**
- *
- * @author mbathily01
- */
 
 import java.io.IOException;
 import java.io.Writer;
 import javafx.scene.canvas.GraphicsContext;
-import javafx.scene.paint.Color;
-import recup.Lire;
+//import javafx.scene.paint.Color;
 
-public class Point extends FigureSimple {
+/**
+ *
+ * @author maxim
+ */
 
+public class Point{
+    
     public static double RAYON_IN_DRAW = 5;
 
     private double px;
     private double py;
+    private Numeroteur<Point> num;
 
-    public Point(double px, double py, Color couleur) {
-        super(couleur);
+    public Point(double px, double py) {
         this.px = px;
         this.py = py;
     }
 
-    public Point(double px, double py) {
-        this(px, py, Color.BLACK);
+    public Point(double px, double py, Numeroteur<Point> num) {
+        this.px = px;
+        this.py = py;
+        this.num = num;
     }
-
+    
     public Point() {
         this(0, 0);
     }
 
-
+    /**
+     * initialise comme une copie du point
+     * @param modele
+     */
     public Point(Point modele) {
-        this(modele.px,modele.px,modele.getCouleur());
+        this(modele.px,modele.px);
     }
-
+    
+    /**
+     * @return the px
+     */
     public double getPx() {
         return px;
     }
 
+    /**
+     * @param px the px to set
+     */
     public void setPx(double px) {
         this.px = px;
     }
 
+    /**
+     * @return the py
+     */
     public double getPy() {
         return py;
     }
+    
+    public Numeroteur<Point> getNum(){
+        return this.num;
+    }
 
+    /**
+     * @param py the py to set
+     */
     public void setPy(double py) {
         this.py = py;
     }
@@ -70,27 +92,27 @@ public class Point extends FigureSimple {
         return new Point(px, py);
     }
 
-    @Override
+   
     public double maxX() {
         return this.px;
     }
 
-    @Override
+    
     public double minX() {
         return this.px;
     }
 
-    @Override
+  
     public double maxY() {
         return this.py;
     }
 
-    @Override
+   
     public double minY() {
         return this.py;
     }
 
-    @Override
+    
     public double distancePoint(Point p) {
         double dx = this.px - p.px;
         double dy = this.py - p.py;
@@ -98,27 +120,27 @@ public class Point extends FigureSimple {
 
     }
 
-    @Override
+    
     public void dessine(GraphicsContext context) {
-        context.setFill(this.getCouleur());
+        //context.setFill(this.getCouleur());
         context.fillOval(this.px-RAYON_IN_DRAW, this.py-RAYON_IN_DRAW, 2*RAYON_IN_DRAW, 2*RAYON_IN_DRAW);
     }
 
-    @Override
+    
     public void dessineSelection(GraphicsContext context) {
-        context.setFill(Figure.COULEUR_SELECTION);
+        //context.setFill(Figure.COULEUR_SELECTION);
         context.fillOval(this.px-RAYON_IN_DRAW, this.py-RAYON_IN_DRAW, 2*RAYON_IN_DRAW, 2*RAYON_IN_DRAW);
     }
 
-    @Override
-    public void save(Writer w, Numeroteur<Figure> num) throws IOException {
+ 
+    public void save(Writer w, Numeroteur<Point> num) throws IOException {
         if(! num.objExist(this)) {
-            int id = num.creeID(this);
+            int id;
+            id = num.creeNum(this);
             w.append("Point;"+id+";"+this.px+";"+this.py+
-                    ";" + FigureSimple.saveColor(this.getCouleur()) + "\n");
+                    ";" + /** FigureSimple.saveColor(this.getCouleur())**/  "\n");
         }
     }
 
-
-
 }
+        

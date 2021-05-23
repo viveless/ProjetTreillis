10d4afef3ff15d29dc7b13299f5ba1f32f45fc50
MaximeArenas <81745672+MaximeArenas@users.noreply.github.com>
2021-05-23 13:56:25 +0200

    Update AppuiDouble.java

--- src/main/java/fr/insa/bathily/projettreillis2/AppuiDouble.java
+++ src/main/java/fr/insa/bathily/projettreillis2/AppuiDouble.java
@@ -8,26 +8,63 @@ package fr.insa.bathily.projettreillis2;
 
 /**
  *
- * @author mbathily01
+ * @author maxim
  */
+
 public class AppuiDouble extends Appui{
+    private Numeroteur<Noeud> num;
+    private double px;
+    private double py;
+    private double alpha;
 
     public AppuiDouble(Numeroteur<Noeud> num, double x, double y) {
-        super(num, x, y);
+        this.num = new Numeroteur<>();
+        this.px = x;
+        this.py = y;
     }
-   
     
-    public AppuiDouble(Numeroteur<Noeud> num, double []p) {
-        super(num, p, col);
+    public AppuiDouble(double x, double y) {
+        this.px=x;
+        this.py=y;
+    }
+
+    public Numeroteur<Noeud> getNum() {
+        return num;
+    }
+
+    @Override
+    public double getPx() {
+        return px;
+    }
+
+    @Override
+    public double getPy() {
+        return py;
+    }
+
+    @Override
+    public double getAlpha() {
+        return alpha;
+    }
+
+    public void setNum(Numeroteur<Noeud> num) {
+        this.num = num;
+    }
+
+    @Override
+    public void setPx(double px) {
+        this.px = px;
+    }
+
+    @Override
+    public void setPy(double py) {
+        this.py = py;
     }
    
-    public ApppuiDouble(Numeroteur<Noeud> num) {
-        super(num);
-    }
     
     @Override
     public String toString() {
-        return  "Noeud Appui Double {\n" + "id : "+getId()+" ; ("+ this.getPx() +","+ this.getPy() +")\n}";
+        return  "Noeud Appui Double {\n" + "id : "+ getNum() +" ; ("+ this.getPx() +","+ this.getPy() +")\n}";
     }
 
 

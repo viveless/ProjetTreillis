dbc5ab9c5715a2419ce5ac88fb1a68f116afd95e
Doniazedd <82376699+Doniazedd@users.noreply.github.com>
2021-05-27 10:10:07 +0200

    Add files via upload

---
+++ src/main/java/fr/insa/bathily/projettreillis2/Charge.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author donia
+ */
+public class Charge {
+    private double px;
+    private double py;
+    private Noeud n;
+    
+    public Charge(double px, double py, Noeud n){
+        this.px=(double)Math.round(px * 1000) / 1000;
+        this.py=(double)Math.round(py * 1000) /1000;
+        this.n=n;
+    }
+    
+    public String toString(){
+        return "Noeud;"+n.getIdentifiant()+";"+"Fx;"+px+";"+"Fy;"+py+"\n";
+    }
+
+    public double getPx() {
+        return px;
+    }
+
+    public double getPy() {
+        return py;
+    }
+
+    public Noeud getN() {
+        return n;
+    }
+
+    public void setPx(double px) {
+        this.px = px;
+    }
+
+    public void setPy(double py) {
+        this.py = py;
+    }
+
+    public void setN(Noeud n) {
+        this.n = n;
+    }
+    
+    
+}
